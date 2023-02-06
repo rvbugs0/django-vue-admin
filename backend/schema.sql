5da6b7d0423f1b289117b5d6760c4a302a6e3e75
Ravi Ailani <rvbugs0@gmail.com>
2023-02-06 15:31:28 -0600

    Added envs folder in project directory + sql for sensory menu

---
+++ backend/schema.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO dvadmin_system_menu VALUES(19,NULL,'1','1','2023-01-26 09:13:48.069962','2023-01-26 09:13:48.069975','sun-o','Sensory Data',1,0,1,NULL,NULL,NULL,1,0,1,1,NULL);
+INSERT INTO dvadmin_system_menu VALUES(20,NULL,'1','1','2023-01-26 09:17:53.595624','2023-01-26 09:17:53.595668','table','Sensory Table',1,0,0,'/sensory_data_table','sensory_data/index','sensory_table',1,0,1,1,19);
+INSERT INTO dvadmin_system_menu VALUES(21,NULL,'1','1','2023-01-26 10:11:50.674939','2023-01-26 10:11:50.674961','line-chart','Sensory Charts',1,0,0,'/sensory_charts','sensory_data/charts','sensory_charts',1,0,1,1,19);
+
