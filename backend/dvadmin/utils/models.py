8ae76a389da8812dac8665ed03c7f62a0b16bce9
Alex <37908934+asclanta@users.noreply.github.com>
2023-03-20 14:34:51 -0500

    Update models.py

--- backend/dvadmin/utils/models.py
+++ backend/dvadmin/utils/models.py
@@ -49,12 +49,12 @@ class SoftDeleteModel(models.Model):
     软删除模型
     一旦继承,就将开启软删除
     """
-    is_deleted = models.BooleanField(verbose_name="是否软删除", help_text='是否软删除', default=False, db_index=True)
+    is_deleted = models.BooleanField(verbose_name="Soft Delete", help_text='Soft Delete', default=False, db_index=True)
     objects = SoftDeleteManager()
 
     class Meta:
         abstract = True
-        verbose_name = '软删除模型'
+        verbose_name = 'Soft Delete'
         verbose_name_plural = verbose_name
 
     def delete(self, using=None, soft_delete=True, *args, **kwargs):
@@ -71,18 +71,18 @@ class CoreModel(models.Model):
     增加审计字段, 覆盖字段时, 字段名称请勿修改, 必须统一审计字段名称
     """
     id = models.BigAutoField(primary_key=True, help_text="Id", verbose_name="Id")
-    description = models.CharField(max_length=255, verbose_name="描述", null=True, blank=True, help_text="描述")
+    description = models.CharField(max_length=255, verbose_name="Remark", null=True, blank=True, help_text="Remark")
     creator = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_query_name='creator_query', null=True,
-                                verbose_name='创建人', help_text="创建人", on_delete=models.SET_NULL, db_constraint=False)
-    modifier = models.CharField(max_length=255, null=True, blank=True, help_text="修改人", verbose_name="修改人")
-    dept_belong_id = models.CharField(max_length=255, help_text="数据归属部门", null=True, blank=True, verbose_name="数据归属部门")
-    update_datetime = models.DateTimeField(auto_now=True, null=True, blank=True, help_text="修改时间", verbose_name="修改时间")
-    create_datetime = models.DateTimeField(auto_now_add=True, null=True, blank=True, help_text="创建时间",
-                                           verbose_name="创建时间")
+                                verbose_name='Creator', help_text="Creator", on_delete=models.SET_NULL, db_constraint=False)
+    modifier = models.CharField(max_length=255, null=True, blank=True, help_text="Updater", verbose_name="Updater")
+    dept_belong_id = models.CharField(max_length=255, help_text="Department", null=True, blank=True, verbose_name="Department")
+    update_datetime = models.DateTimeField(auto_now=True, null=True, blank=True, help_text="Update Time", verbose_name="Update Time")
+    create_datetime = models.DateTimeField(auto_now_add=True, null=True, blank=True, help_text="Create Time",
+                                           verbose_name="Create Time")
 
     class Meta:
         abstract = True
-        verbose_name = '核心模型'
+        verbose_name = 'Core Model'
         verbose_name_plural = verbose_name
 
 
