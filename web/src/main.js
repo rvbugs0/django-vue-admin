b61ea16f39709bd2e64aaf6c06c8ae9035171fa6
Ravi Ailani <rvbugs0@gmail.com>
2023-04-15 13:08:10 -0500

    Added gmap

--- web/src/main.js
+++ web/src/main.js
@@ -7,52 +7,79 @@
  * @文件介绍:
  */
 // Vue
-import Vue from 'vue'
-import i18n from './i18n'
-import App from './App'
+import Vue from "vue";
+import i18n from "./i18n";
+import App from "./App";
 // 核心插件
-import d2Admin from '@/plugin/d2admin'
+import d2Admin from "@/plugin/d2admin";
 // store
-import store from '@/store/index'
+import store from "@/store/index";
 
 // 菜单和路由设置
-import router from './router'
-import { menuHeader } from '@/menu'
+import router from "./router";
+import { menuHeader } from "@/menu";
 
 // 按钮权限
-import '@/plugin/permission' // 加载permission
+import "@/plugin/permission"; // 加载permission
 
 // d2-crud-plus 安装与初始化
-import './install'
+import "./install";
 // 配置vxe-table
-import 'xe-utils'
-import VXETable from 'vxe-table'
-import 'vxe-table/lib/style.css'
+import "xe-utils";
+import VXETable from "vxe-table";
+import "vxe-table/lib/style.css";
 
 // md5加密
-import md5 from 'js-md5'
+import md5 from "js-md5";
 
 // websocket
-import websocket from '@/api/websocket'
+import websocket from "@/api/websocket";
 
 // 核心插件
-Vue.use(d2Admin)
-Vue.use(VXETable)
-Vue.prototype.$md5 = md5
-Vue.prototype.$websocket = websocket
+Vue.use(d2Admin);
+Vue.use(VXETable);
+Vue.prototype.$md5 = md5;
+Vue.prototype.$websocket = websocket;
+
+
+import * as VueGoogleMaps from 'vue2-google-maps'
+ 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg',
+    libraries: 'places,marker', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+ 
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+ 
+  //// If you intend to programmatically custom event listener code
+  //// (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+  //// instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+  //// you might need to turn this on.
+  // autobindAllEvents: false,
+ 
+  //// If you want to manually install components, e.g.
+  //// import {GmapMarker} from 'vue2-google-maps/src/components/marker'
+  //// Vue.component('GmapMarker', GmapMarker)
+  //// then disable the following:
+  // installComponents: true,
+});
 
 new Vue({
   router,
   store,
   i18n,
-  render: h => h(App),
-  beforeCreate () {
+  render: (h) => h(App),
+  beforeCreate() {
     // 初始化配置
-    this.$store.dispatch('d2admin/settings/load')
-    this.$store.dispatch('d2admin/dictionary/load')
+    this.$store.dispatch("d2admin/settings/load");
+    this.$store.dispatch("d2admin/dictionary/load");
   },
-  created () {
-
+  created() {
     // 处理路由 得到每一级的路由设置
     // this.$store.commit('d2admin/page/init', frameInRoutes)
     // 设置顶栏菜单
@@ -62,28 +89,30 @@ new Vue({
     // 初始化菜单搜索功能
     // this.$store.commit('d2admin/search/init', menuAside)
   },
-  mounted () {
+  mounted() {
     // 展示系统信息
-    this.$store.commit('d2admin/releases/versionShow')
+    this.$store.commit("d2admin/releases/versionShow");
     // 用户登录后从数据库加载一系列的设置
-    this.$store.dispatch('d2admin/account/load')
+    this.$store.dispatch("d2admin/account/load");
     // 获取并记录用户 UA
-    this.$store.commit('d2admin/ua/get')
+    this.$store.commit("d2admin/ua/get");
     // 初始化全屏监听
-    this.$store.dispatch('d2admin/fullscreen/listen')
+    this.$store.dispatch("d2admin/fullscreen/listen");
   },
   watch: {
     // 检测路由变化切换侧边栏内容
-    '$route.matched': {
-      handler (matched) {
+    "$route.matched": {
+      handler(matched) {
         if (matched.length > 0) {
-          const _side = menuHeader.filter(menu => menu.path === matched[0].path)
+          const _side = menuHeader.filter(
+            (menu) => menu.path === matched[0].path
+          );
           if (_side.length > 0) {
-            this.$store.commit('d2admin/menu/asideSet', _side[0].children)
+            this.$store.commit("d2admin/menu/asideSet", _side[0].children);
           }
         }
       },
-      immediate: true
-    }
-  }
-}).$mount('#app')
+      immediate: true,
+    },
+  },
+}).$mount("#app");
---
+++ web/src/views/sensory_data/chart_config/location_chart.vue
@@ -0,0 +1,44 @@
+<script>
+
+import * as VueGoogleMaps from 'vue2-google-maps';
+
+export default {
+    name: "location_chart",
+
+    data() {
+        return {
+            mapCenter: { lat: 32.7292117  , lng: -97.1151971}, // Default map center (San Francisco)
+            mapZoom: 12, // Default map zoom level
+            markerPosition: { lat: 32.7292117, lng: -97.1151971 }, // Default marker position (San Francisco)
+            markerTitle:"UTA"
+        };
+    },
+    mounted() {
+        this.$refs.mapRef.$mapPromise.then((map) => {
+            map.panTo({ lat: this.mapCenter.lat, lng: this.mapCenter.lng })
+        })
+    }
+    , components: {
+        GmapMap: VueGoogleMaps.Map,
+        GmapMarker: VueGoogleMaps.Marker,
+    }
+}
+</script>
+<template>
+    <div>
+        <div style="z-index: 1000; width: 500px; height: 500px;">
+            <gmap-map ref="mapRef" :center="mapCenter" :zoom="mapZoom" style="height: 400px; width: 100%">
+                <gmap-marker :position="markerPosition" :label="markerTitle"></gmap-marker>
+            </gmap-map>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss">
+.yxtInput {
+    .el-form-item__label {
+        color: #49a1ff;
+    }
+}
+</style>
